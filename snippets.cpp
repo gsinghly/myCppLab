3ad5a1768655a8bb6e08890c89e2b319f686f256
Gaurav Singh <35264030+saintpopo@users.noreply.github.com>
2020-09-19 07:12:19 +0530

    Update snippets.cpp

--- snippets.cpp
+++ snippets.cpp
@@ -1,1 +1,27 @@
+### Power Mod (a^b)%c
+
+ll power(ll a, ll b, ll c){
+  ll ans = 1;
+  a = a%c;
+  while(y > 0){
+    if(y&1){
+      ans = (ans*c)%c;
+    }
+    y >>= 1;
+    x = (x*x)%c;
+  }
+  return ans;
+}
+
+
+
+
+### Mul Mod (a*b)%c; a, b, c <= 1e18
+
+ll mulmod(ll a, ll b, ll c){
+  if(b == 0)  return 0;
+  ll s = mulmod(a, b/2, c);
+  if(b&1) return (a%c * 2*(s%c))%c;
+  return (2*(s%c))%c;
+}
 
